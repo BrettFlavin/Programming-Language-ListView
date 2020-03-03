05d60365b6b518d108af089b07df7749187148b6
Brett Flavin <brettflavin@yahoo.com>
2020-03-02 21:15:47 -0800

    Initial Commit - main view model based on Languages class implemented on HomePage.

---
+++ HW4_List/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HW4_List
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new HomePage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ HW4_List/Language.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW4_List
+{
+    public class Language
+    {
+        public string Name { get; set; }          // name of the programming language
+        public string ReleaseDate { get; set; }   // initial release date of the language
+        public string Description { get; set; }   // short decription of the language
+        public string IconURL { get; set; }       // language's logo
+        public string ExampleURL { get; set; }    // language example code image
+    }
+}
